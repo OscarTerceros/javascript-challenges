3ab0dd8d7db682148023e2798ce4949b1154ca2d
Oscar Terceros <osterce@gmail.com>
2022-09-06 23:19:57 -0400

    Reto terminado

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = (n) => {
+const fibonacci = (number) => {
   // your code here
+  let primerNumero = 0, segundoNumero = 1, siguienteTermino, resultado = [];
+
+  for (let i = 1; i <= number; i++) {
+    siguienteTermino = primerNumero + segundoNumero;
+    primerNumero = segundoNumero;
+    segundoNumero = siguienteTermino;
+    resultado.push(primerNumero);
+  }
+  return resultado;
 }
 
 module.exports = fibonacci;
